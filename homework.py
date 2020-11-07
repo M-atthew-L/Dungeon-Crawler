e3872d463b3aa723214b978ac8099b8806bc681b
M-atthew-L <matthewlee080807@gmail.com>
2020-11-07 13:33:25 -0800

    Homeword

---
+++ homework.py
@@ -0,0 +1,40 @@
+import Math
+#Question 1
+variables = []
+quadratic_Equation = (1,2,-3)
+variables.append(quadratic_Equation.split(,))
+firstPart = variables[0] * 
+pass
+
+#Question 2
+def fraction(self):
+    sum_fractions([[18, 13], [4, 5]]) ➞ 2
+    fractionPartList = []
+    fractionPartList = sum_fractions.split(",")
+    a = fractionPartList[0][0]
+    b = fractionPartList[0][1]
+    c = fractionPartList[1][0]
+    d = fractionPartList[1][1]
+    if b != d:
+        e = b * d
+        firstFraction = a / e
+        secondFraction = c / e
+        finalNumerator = a + c
+        finalAnswer = finalNumerator / e
+    else: 
+        firstFraction = a / b
+        secondFraction = c / d
+        finalNumerator = a + c
+        finalAnswer = finalNumerator / b
+
+#Question 3
+def findLongestWord(self):
+    longest_word("Margaret's toy is a pretty doll.")
+    longest_word = []
+    longestWord = longest_word.split(" ")
+    for word in len(longestWord):
+        currentLongestWord = 0
+        if len(word) > lengthOfLongestWord:
+            lengthOfLongestWord = len(word)
+            longestWord = word
+    print(currentLongestWord)
